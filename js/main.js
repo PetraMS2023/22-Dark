47606222b6dbb77c3211ac6b206027d44b499eeb
Petra Ms <petrams.200@gmail.com>
2024-11-07 09:54:12 +0300

    add new site

---
+++ js/main.js
@@ -0,0 +1,53 @@
+let links = document.querySelectorAll('.link')
+
+
+links.forEach((e)=>{
+  e.addEventListener('click',()=>{
+    links.forEach((e)=>{e.classList.remove('active')})
+    e.classList.add('active')
+  })
+})
+
+let drop = document.querySelectorAll('.drop');
+
+drop.forEach((e)=>{
+  e.addEventListener('click',()=>{
+    e.classList.toggle('active');
+  })
+})
+
+
+let header = document.querySelector(".header");
+window.addEventListener('scroll',()=>{
+  if(window.scrollY >= 100){
+    header.classList.add('color');
+  }else{
+    header.classList.remove('color');
+  }
+})
+
+let homeMedia = document.getElementById('homeMedia');
+if(homeMedia){
+  homeMedia.addEventListener('click',()=>{
+    homeMedia.classList.toggle('active');
+  })
+  
+  if(window.innerWidth <= 421){
+    homeMedia.addEventListener('mouseleave',()=>{
+      homeMedia.classList.remove('active');
+    })
+  }
+}
+
+
+let nav = document.querySelector(".navCont");
+let menu = document.querySelector(".menu");
+
+menu.addEventListener("click", () => {
+  if (nav.style.left === "0px") {
+    nav.style.left = "-800px";
+  } else {
+    nav.style.left = "0px";
+  }
+  menu.classList.toggle("active");
+});
